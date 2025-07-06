d93ed9cae277d3764a7a38f4a148c05a2b203c15
robel75 <robelisgone2@gmail.com>
2025-07-06 21:02:41 +0300

    Create 0-stream_users.py

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+def stream_users():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root", # Change this if needed
+        password="your_password", # Replace with your actual password
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
