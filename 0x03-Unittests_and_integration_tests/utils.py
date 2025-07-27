b0f2909a9e255c09f6a0208e9e0198322a7918ad
robel75 <robelisgone2@gmail.com>
2025-07-27 21:11:29 +0300

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python3
-"""Utils module"""
-import requests
-from functools import wraps
+"""Utilities module"""
 
+def memoize(method):
+    """Memoization decorator"""
+    attr_name = "_memoized_" + method.name
 
-def access_nested_map(nested_map, path):
-    """Access nested map with a path"""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
-
-
-def get_json(url):
-    """Get JSON from URL"""
-    response = requests.get(url)
-    return response.json()
-
-
-def memoize(fn):
-    """Memoize decorator"""
-    attr_name = "_{}".format(fn.name)
-
-    @wraps(fn)
+    @property
     def wrapper(self):
         if not hasattr(self, attr_name):
-            setattr(self, attr_name, fn(self))
+            setattr(self, attr_name, method(self))
         return getattr(self, attr_name)
+
+    return wrapper
 
     return wrapper
