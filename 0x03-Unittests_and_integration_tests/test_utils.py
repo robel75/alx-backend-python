b853de63a16c3074ffcc2f17b81dcfa2480bc018
robel75 <robelisgone2@gmail.com>
2025-07-30 18:10:40 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.py."""
+"""Unit test for the memoize function."""
 
 import unittest
 from unittest.mock import patch
-from utils import access_nested_map, memoize
-
-
-class TestAccessNestedMap(unittest.TestCase):
-    """Test the access_nested_map function."""
-
-    def test_access_nested_map(self):
-        """Test with normal inputs."""
-        self.assertEqual(access_nested_map({"a": 1}, ("a",)), 1)
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a",)), {"b": 2})
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a", "b")), 2)
-
-    def test_access_nested_map_exception(self):
-        """Test that KeyError is raised for missing keys."""
-        with self.assertRaises(KeyError):
-            access_nested_map({}, ("a",))
-        with self.assertRaises(KeyError):
-            access_nested_map({"a": 1}, ("a", "b"))
+from utils import memoize
 
 
 class TestMemoize(unittest.TestCase):
@@ -41,7 +24,3 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(obj.a_property(), 42)
             self.assertEqual(obj.a_property(), 42)
             mocked.assert_called_once()
-
-
-if __name__ == __'main'__:
-    unittest.main()
