4fd9af1a492d289da5e2d95e2d27520ab3d9cc96
Robel <robelisgone2@gmail.com>
2025-08-12 12:46:11 +0300

    1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,8 +12,12 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected ):
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-
+    @parameterized.expand([
+    ({}, {"a",}, KeyError),
+    ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map,path,expected):
+        self.assertRaises(access_nested_map(nested_map,path),expected)
 
 if __name__ == "__main__":
     unittest.main()
