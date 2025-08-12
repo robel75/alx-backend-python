6bf824636c48a37f60d00c2926e16c26ca0dca9a
Robel <robelisgone2@gmail.com>
2025-08-12 12:03:21 +0300

    first commit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-"""Unit test for the memoize function."""
-
 import unittest
-from unittest.mock import patch
-from utils import memoize
+from parameterized import parameterized
+from utils import access_nested_map
 
 
-class TestMemoize(unittest.TestCase):
-    """Tests for the memoize decorator."""
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+    ({"a": 1}, ("a",), 1),
+    ({"a": {"b": 2}}, ("a",), {"b": 2}),
+    ({"a":{"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected ):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    def test_memoize(self):
-        """Test that memoize caches the result of a method."""
-        class TestClass:
-            def a_method(self):
-                return 42
 
-            @memoize
-            def a_property(self):
-                return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mocked:
-            obj = TestClass()
-            self.assertEqual(obj.a_property, 42)
-            self.assertEqual(obj.a_property, 42)
-            mocked.assert_called_once()
+if __name__ == "__main__":
+    unittest.main()
+
+
+  
+    
+  
