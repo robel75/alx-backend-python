c848bab616ceff9d0b5b54a85263bfe893afa96b
Robel <robelisgone2@gmail.com>
2025-08-14 11:52:06 +0300

    updated

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -86,8 +86,10 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/torr/repos")
     @parameterized.expand([
-        ({"license": {"key": "my_license"}},"my_license",True),
-        ({"license": {"key": "other_license"}},"my_license",False)
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({},"my_license", False),
+        ({"license":None}, "my_license", False)
         ])
     def test_has_license(self, repo, license_key, expected):
         client = GithubOrgClient("testorgs")
