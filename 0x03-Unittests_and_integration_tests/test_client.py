d697117bcd020d741ba42af170344e03584e6002
Robel <robelisgone2@gmail.com>
2025-08-14 11:43:44 +0300

    initial'

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -85,6 +85,14 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_url.assert_called_once()
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/torr/repos")
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}},"my_license",True),
+        ({"license": {"key": "other_license"}},"my_license",False)
+        ])
+    def test_has_license(self, repo, license_key, expected):
+        client = GithubOrgClient("testorgs")
+        result = client.has_license(repo,license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
