21723a2056a4b4020f804a5082a692802a4d995f
Robel <robelisgone2@gmail.com>
2025-08-20 19:10:15 +0300

    up

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -2,10 +2,20 @@ import django_filters
 from .models import Message
 
 class MessageFilter(django_filters.FilterSet):
-    start_date = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr="gte")
-    end_date = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr="lte")
-    sender = django_filters.CharFilter(field_name="sender__username", lookup_expr="icontains")
+    """
+    Filter messages by sender username or by time range (start_date, end_date).
+    """
+    sender = django_filters.CharFilter(
+        field_name="sender__username", lookup_expr="icontains"
+    )
+    start_date = django_filters.DateTimeFilter(
+        field_name="sent_at", lookup_expr="gte"
+    )
+    end_date = django_filters.DateTimeFilter(
+        field_name="sent_at", lookup_expr="lte"
+    )
 
     class Meta:
         model = Message
         fields = ["sender", "start_date", "end_date"]
+
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",  # custom pagination
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",  # custom pagination
     "PAGE_SIZE": 20,  # 20 messages per page
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
