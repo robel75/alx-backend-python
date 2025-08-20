2cb4aa3c2fb542de8c5338ce8accce22b8dc36a2
Robel <robelisgone2@gmail.com>
2025-08-20 10:27:33 +0300

    up

---
+++ messaging_app/chats/permission.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+class IsConversationParticipant(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # obj could be Conversation or Message
+        if hasattr(obj, "participants"):  # Conversation
+            return obj.participants.filter(id=request.user.id).exists()
+        if hasattr(obj, "conversation"):  # Message
+            return obj.conversation.participants.filter(id=request.user.id).exists()
+        return False
