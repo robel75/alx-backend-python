dc759eb71d7e305148283bf1f2f5cb930c8cdd18
Robel <robelisgone2@gmail.com>
2025-08-22 12:06:34 +0300

    up

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -15,4 +15,14 @@ class RequestLoggingMiddleware:
         response = self.get_response(request)
         return response
 
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response= get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        if (18,0)<datetime.now().time()<(21,0):
+            raise PermissionDenied
+
         
