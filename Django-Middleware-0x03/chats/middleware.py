df6b73964619b88c8b5737396d3a52787fdaf9c0
Robel <robelisgone2@gmail.com>
2025-08-22 11:02:53 +0300

    up

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.core.exceptions import permissionDenied
+from datetime import datetime
+import logging
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        with open("requests.log", "a")as f:
+            f.write(f"{datetime.now()} user : {user}, path: {request.path}\n")
+
+        response = self.get_response(request)
+        return response
+
+        
