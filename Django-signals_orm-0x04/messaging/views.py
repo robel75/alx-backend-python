a6c2b5d700f4b0955ef0ba545bc726fa473a0edf
Robel <robelisgone2@gmail.com>
2025-08-26 12:58:49 +0300

    up

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -44,7 +44,7 @@ def conversation_view(request):
 
 @login_required
 def inbox_view(request):
-    unread_messages = Message.unread.for_user(request.user)
+    unread_messages = Message.unread.unread_for_user(request.user)
     return render(request, 'messaging/inbox.html', {'unread_messages': unread_messages})
 
 
